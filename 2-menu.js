794e57b976bb17b5e078f212c1bdb3d012ce9c7d
Myungsan Yun <88599454+myungsan@users.noreply.github.com>
2023-04-26 15:38:28 -0700

    230426

---
+++ 2-menu.js
@@ -0,0 +1,9 @@
+function onClickMember() {
+    window.location.href = "3-member-introduction.html";
+    }
+
+function onClickHistory() {
+    window.location.href = "https://quiver-ceiling-a5b.notion.site/History-9f096f2e7b1c4891a2a24295f0a0c9e6";
+    }
+
+    
